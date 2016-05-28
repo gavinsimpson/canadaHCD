5f2a60d14c1c8f14258f2ca25edb041303b5824b
Gavin Simpson <ucfagls@gmail.com>
2016-05-28 14:09:02 -0600

    add a test for multiple years in hcd_daily()

--- tests/testthat/test-hcd_daily.R
+++ tests/testthat/test-hcd_daily.R
@@ -4,14 +4,14 @@ library("canadaHCD")
 context("Test `hcd_daily()`")
 
 ## test downloading of file from HCD dataset
-test_that("hcd_daily(n) returns a tbl_df", {
+test_that("hcd_daily() returns a tbl_df", {
     skip_on_cran()
     df <- hcd_daily("2855", 2015)
     expect_that(df, is_a("tbl_df"))
 })
 
 ## test downloading of multiple files from HCD dataset
-test_that("hcd_daily(n) works with multiple stations", {
+test_that("hcd_daily() works with multiple stations", {
     skip_on_cran()
     df <- hcd_daily(c("2855", "2855"), year = 2015)
     expect_that(df, is_a("tbl_df"))
@@ -20,5 +20,18 @@ test_that("hcd_daily(n) works with multiple stations", {
     expect_identical(length(df), 2L)
     expect_that(df[[1]], is_a("tbl_df"))
     expect_that(df[[2]], is_a("tbl_df"))
+})
+
+## test downloading of multiple years from a single HCD dataset
+test_that("hcd_daily() works with multiple years", {
+    skip_on_cran()
+    df <- hcd_daily("2855", year = 2013:2015)
+    expect_that(df, is_a("tbl_df"))
+    df <- hcd_daily("2855", year = 2013:2015, collapse = FALSE)
+    expect_that(df, is_a("list"))
+    expect_identical(length(df), 3L)
+    expect_that(df[[1]], is_a("tbl_df"))
+    expect_that(df[[2]], is_a("tbl_df"))
+    expect_that(df[[3]], is_a("tbl_df"))
 })
 
