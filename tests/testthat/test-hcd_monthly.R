1a99908dea0a577ac79d6e986f35c1ccb996aa4a
Gavin Simpson <ucfagls@gmail.com>
2016-05-28 11:45:32 -0600

    use a data file we know works

--- tests/testthat/test-hcd_monthly.R
+++ tests/testthat/test-hcd_monthly.R
@@ -13,9 +13,9 @@ test_that("hcd_monthly(n) returns a tbl_df", {
 ## test downloading of multiple files from HCD dataset
 test_that("hcd_monthly(n) works with multiple stations", {
     skip_on_cran()
-    df <- hcd_monthly(c("51441", "2925"))
+    df <- hcd_monthly(c("2855", "2855"))
     expect_that(df, is_a("tbl_df"))
-    df <- hcd_monthly(c("51441", "2925"), collapse = FALSE)
+    df <- hcd_monthly(c("2855", "2855"), collapse = FALSE)
     expect_that(df, is_a("list"))
     expect_identical(length(df), 2L)
     expect_that(df[[1]], is_a("tbl_df"))
