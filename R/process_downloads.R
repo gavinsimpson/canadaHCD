94060ee9ee78ff90de64b918312a14342a889fb2
Gavin Simpson <ucfagls@gmail.com>
2016-05-28 18:59:15 -0600

    move repeated download code to a function

---
+++ R/process_downloads.R
@@ -0,0 +1,16 @@
+##' @importFrom utils txtProgressBar setTxtProgressBar
+`process_downloads` <- function(urls, progress = TRUE, ...) {
+    nurls <- length(urls)
+    sdata <- vector(mode = "list", length = nurls)
+    if (isTRUE(progress)) {
+        pb <- txtProgressBar(min = 0, max = nurls, style = 3)
+    }
+    on.exit(close(pb))
+    for (i in seq_along(sdata)) {
+        sdata[[i]] <- get_hcd_from_url(urls[i], ...)
+        if (isTRUE(progress)) {
+            setTxtProgressBar(pb, i)
+        }
+    }
+    sdata
+}
