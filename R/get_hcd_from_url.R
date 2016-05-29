9e0b74334a6bf2d220bda4ea60a2736266abe6f6
Gavin Simpson <ucfagls@gmail.com>
2016-05-28 20:06:50 -0600

    capture parse warnings and return them as attributes on the tbl_df

--- R/get_hcd_from_url.R
+++ R/get_hcd_from_url.R
@@ -1,6 +1,8 @@
 get_hcd_from_url <- function(URL, ...) {
     tmp <- tempfile()
     f <- curl_download(URL, destfile = tmp)
-    df <- read_hcd(f, ...)
+    res <- capture_warnings(read_hcd(f, ...))
+    df <- res$value
+    attr(df, "warnings") <- res$warnings
     df
 }
