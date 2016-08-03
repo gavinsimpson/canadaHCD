0a3bb18b2fcdf38e4a59497b5cdb297e45818214
Conor Anderson <conor.anderson@gmail.com>
2016-08-03 18:11:33 -0400

    Add a function to search by proximity to a target station

---
+++ R/find_proximity.R
@@ -0,0 +1,37 @@
+##' @title Find a Climate Data station by proximity to a target
+##'
+##' @description Search for stations in the Historical Climate Data inventory by proximity to a target station.
+##'
+##' @param target numeric; number of the target (reference) station
+##' @param mindist numeric; minimum distance from the target station (defaults to 0)
+##' @param maxdist numeric; maximum distance from the target station (defaults to 100)
+##' @param sort Boolean; if set to TRUE (default), will sort the resultant table by distance from `target`
+##'
+##' @return An object of class \code{"hcd_station_list"}, which is a \code{"tbl_df"}, containing details of any matching HCD stations.
+##'
+##' @import geosphere 
+##'
+##' @export
+##'
+##' @author Conor I. Anderson
+##'
+##' @examples
+##' find_proximity(5051)
+
+find_proximity <- function(target, mindist = 0, maxdist = 100, sort = TRUE) {
+  
+  tempdata <- station_data
+  p1 <- c(tempdata$LongitudeDD[grep(paste0("\\b", as.character(target), "\\b"), tempdata$StationID)], tempdata$LatitudeDD[grep(paste0("\\b", as.character(target), "\\b"), tempdata$StationID)])
+  library(geosphere)
+  
+  tempdata$LongLat <- rep(NA, nrow(tempdata))
+  tempdata$Dist <- rep(NA, nrow(tempdata))
+  
+  for (i in 1:nrow(tempdata)) {
+    tempdata$Dist[i] <- (geosphere::distGeo(p1, c(tempdata$LongitudeDD[i], tempdata$LatitudeDD[i]))/10000)
+  }
+  tempdata <- tempdata[(!is.na(tempdata$Dist) & (tempdata$Dist >= mindist) & (tempdata$Dist <= maxdist)),]
+  if (sort == TRUE) tempdata <- tempdata[order(tempdata$Dist),]
+  return(tempdata)
+  
+}
