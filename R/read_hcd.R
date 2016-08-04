fb1c6ff4a56a9e9e11e0bd4f9f86a8dd85695599
Conor Anderson <conor.anderson@gmail.com>
2016-08-04 15:08:27 -0400

    fixes an issue causing downloads to fail

--- R/read_hcd.R
+++ R/read_hcd.R
@@ -11,9 +11,10 @@
 ##'
 ##' @author Gavin L. Simpson
 ##'
-##' @importFrom readr read_csv count_fields locale tokenizer_csv type_convert
+##' @importFrom readr read_csv locale tokenizer_csv type_convert
 ##' @importFrom tibble as_data_frame
 ##' @importFrom zoo as.yearmon
+##' @importFrom utils count.fields
 ##'
 ##' @export
 ##'
@@ -25,7 +26,7 @@
 ##' read_hcd(system.file("extdata/2855-daily-data-2015.csv", package = "canadaHCD"))
 `read_hcd` <- function(file, flags = FALSE, clean = TRUE, ...) {
     Date <- NULL                        # to kill the global variable NOTE!
-    nfields <- count_fields(file, tokenizer_csv(), n_max = 26L)
+    nfields <- count.fields(file, sep = ",", blank.lines.skip = FALSE)[1:26]
     SKIP <- which(nfields == max(nfields))[1L] - 1L
     types <- ifelse(SKIP == 25L, paste0("Diiic", paste0(rep("dc", 8L), collapse = ""), "iciccc"),
              ifelse(SKIP == 18L, paste0("cii", paste0(rep("dc", 8L), collapse = ""), "iciccc"),
