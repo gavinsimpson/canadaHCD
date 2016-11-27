ac560a86378eaf6687e898a3d97459481516cc9b
Gavin Simpson <ucfagls@gmail.com>
2016-11-26 20:13:55 -0600

    fix the test to only download the single site data set once

--- tests/testthat/test-hcd_monthly.R
+++ tests/testthat/test-hcd_monthly.R
@@ -3,10 +3,11 @@ library("canadaHCD")
 
 context("Test `hcd_monthly()`")
 
+df <- hcd_monthly("2855")
+
 ## test downloading of file from HCD dataset
 test_that("hcd_monthly(n) returns a tbl_df", {
     skip_on_cran()
-    df <- hcd_monthly("2855")
     expect_that(df, is_a("tbl_df"))
 })
 
@@ -19,11 +20,11 @@ test_that("hcd_monthly(n) Date variables is class 'yearmon'", {
 ## test downloading of multiple files from HCD dataset
 test_that("hcd_monthly(n) works with multiple stations", {
     skip_on_cran()
-    df <- hcd_monthly(c("2855", "2855"))
-    expect_that(df, is_a("tbl_df"))
-    df <- hcd_monthly(c("2855", "2855"), collapse = FALSE)
-    expect_that(df, is_a("list"))
-    expect_identical(length(df), 2L)
-    expect_that(df[[1]], is_a("tbl_df"))
-    expect_that(df[[2]], is_a("tbl_df"))
+    dfs <- hcd_monthly(c("2855", "2855"))
+    expect_that(dfs, is_a("tbl_df"))
+    dfs <- hcd_monthly(c("2855", "2855"), collapse = FALSE)
+    expect_that(dfs, is_a("list"))
+    expect_identical(length(dfs), 2L)
+    expect_that(dfs[[1]], is_a("tbl_df"))
+    expect_that(dfs[[2]], is_a("tbl_df"))
 })
