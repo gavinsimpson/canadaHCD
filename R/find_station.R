36972716bf6e91d38d58b5fbadfbb765ad368726
Gavin Simpson <ucfagls@gmail.com>
2017-01-17 15:58:49 -0600

    add option to allow globs in regexp pattern via glob2rx: closes #16

--- R/find_station.R
+++ R/find_station.R
@@ -4,9 +4,12 @@
 ##'
 ##' @param name character; character string or a regular expression to be matched against known station names. See \code{\link{grep}} for details.
 ##' @param ignore.case logical; by default the search for station names is not case-sensitive.
+##' @param glob logical; use wildcards (e.g. \code{"yell*"}) in the \code{name}. See \code{link{glob2rx}} for details.
 ##' @param ... Additional arguments passed to \code{\link{grep}}.
 ##'
 ##' @return An object of class \code{"hcd_station_list"}, which is a \code{"tbl_df"}, containing details of any matching HCD stations.
+##'
+##' @importFrom utils glob2rx
 ##'
 ##' @export
 ##'
@@ -16,7 +19,13 @@
 ##' find_station("Regina")
 ##'
 ##' find_station("Yellowknife")
-`find_station` <- function(name = NULL, ignore.case = TRUE, ...) {
+##'
+##' find_station("Yell*", glob = TRUE)
+`find_station` <- function(name = NULL, ignore.case = TRUE, glob = FALSE,
+                           ...) {
+    if (glob) {
+        name <- glob2rx(name)
+    }
     take <- grep(name, station_data$Name, ignore.case = ignore.case, ...)
     vars_wanted <- c("Name", "Province", "StationID", "LatitudeDD", "LongitudeDD")
     df <- station_data[take, vars_wanted]
