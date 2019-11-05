41ec4b0fcefd33c61f34ddfb4d890092f625c046
Gavin Simpson <ucfagls@gmail.com>
2019-11-04 18:34:53 -0600

    twek a test

--- tests/testthat/test-read_hcd.R
+++ tests/testthat/test-read_hcd.R
@@ -7,7 +7,5 @@ context("Test `read_hcd()`")
 test_that("read_hcd() returns a tbl_df", {
     df <- read_hcd(system.file("extdata/2855-monthly-data.csv",
                                package = "canadaHCD"))
-    expect_that(df, is_a("tbl_df"))
+    expect_s3_class(df, "tbl_df")
 })
-
-
