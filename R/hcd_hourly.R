21746a6d598b89d89c39e33292186e714859d41b
gavinsimpson <ucfagls@gmail.com>
2023-09-18 09:05:34 +0000

    Deploying to gh-pages from @ gavinsimpson/canadaHCD@cfba770fc513f5c70eec6e46714e51ab8bfdbe64 🚀

--- R/hcd_hourly.R
+++ R/hcd_hourly.R
@@ -1,19 +1,24 @@
-##' @title Download hourly Historical Climate Data records
-##'
-##' @description Hourly Historical Climate Data for stated station IDs are downloaded from the Government of Canada's Historical Climate Data website.
-##' @param station Character or numeric; one or more HCD station IDs.
-##' @param year numeric; vector of one or more years
-##' @param month numeric; vector of one or months
-##' @param collapse Logical; if \code{TRUE} and multiple \code{station}s are requested a single \code{data frame} is returned, formed by row-binding the data for each station and prepending a station identifier variable.
-##' @param progress Logical; if \code{TRUE}, a bar is shown indicating progress in downloading station data from the HCD website.
-##' @param ... Further arguments passed to \code{\link{read_hcd}}.
-##'
-##' @return A \code{\link{tbl_df}} containing the requested monthly climate data
-##'
-##' @author Gavin L. Simpson
-##'
-##' @export
-`hcd_hourly` <- function(station, year, month, collapse = TRUE, progress = TRUE, ...) {
+#' @title Download hourly Historical Climate Data records
+#'
+#' @description Hourly Historical Climate Data for stated station IDs are
+#'   downloaded from the Government of Canada's Historical Climate Data website.
+#' @param station Character or numeric; one or more HCD station IDs.
+#' @param year numeric; vector of one or more years
+#' @param month numeric; vector of one or months
+#' @param collapse Logical; if \code{TRUE} and multiple \code{station}s are
+#'   requested a single \code{data frame} is returned, formed by row-binding
+#'   the data for each station and prepending a station identifier variable.
+#' @param progress Logical; if \code{TRUE}, a bar is shown indicating progress
+#'   in downloading station data from the HCD website.
+#' @param ... Further arguments passed to \code{\link{read_hcd}}.
+#'
+#' @return A \code{\link{tbl_df}} containing the requested monthly climate data
+#'
+#' @author Gavin L. Simpson
+#'
+#' @export
+`hcd_hourly` <- function(station, year, month, collapse = TRUE,
+    progress = TRUE, ...) {
     ## Generate URLs
     urls <- hcd_url(station, timescale = "hourly", year = year, month = month)
     ## Download data
@@ -25,4 +30,3 @@
 
     sdata
 }
-
