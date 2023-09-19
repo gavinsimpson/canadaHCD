58c4cd7073403c542f377d60263d7b869a6b352a
Gavin Simpson <ucfagls@gmail.com>
2023-09-19 11:38:55 +0200

    test globbing

--- tests/testthat/test-find_station.R
+++ tests/testthat/test-find_station.R
@@ -9,3 +9,12 @@ test_that("find_station() can locate a station", {
     expect_s3_class(df, "data.frame")
     expect_equal(nrow(df), 6L)
 })
+
+## test finding a station by name with globbing
+test_that("find_station() can locate a station", {
+    df <- find_station("Yell*", glob = TRUE)
+    expect_s3_class(df, "hcd_station_list")
+    expect_s3_class(df, "tbl_df")
+    expect_s3_class(df, "data.frame")
+    expect_equal(nrow(df), 9L)
+})
